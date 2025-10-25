aecc5a48ad3fcd309bc2b01ffb120eaaed6c7611
RelativelyBigMan <cillianberry75@gmail.com>
2025-10-25 18:22:31 +0100

    added better graphing

--- load_test_data.py
+++ load_test_data.py
@@ -6,10 +6,17 @@ from statistics import mean,mode,median
 with open("chess_games.json") as f:
     games = json.load(f)
 
+with open("chess_games_alpha_beta_pruning.json") as f:
+    alpha_beta_pruning = json.load(f)
+
 
 moves = [g["moves"] for g in games]
 durations = [g["duration"] for g in games]
 endings = [g["ending"] for g in games]
+
+moves2 = [g["moves"] for g in alpha_beta_pruning]
+durations2 = [g["duration"] for g in alpha_beta_pruning]
+endings2 = [g["ending"] for g in alpha_beta_pruning]
 
 
 meanDuration = mean(durations)
@@ -20,7 +27,7 @@ modeDuration = mode(durations)
 perctange_of_insufficent_to_checkmate = (endings.count("checkmate")/ endings.count("insufficient_pieces")) * 100
 plt.bar(("insufficient_pieces", "Checkmate"), (endings.count("insufficient_pieces"), endings.count("checkmate")))
 plt.text(0.5, 540, f"{perctange_of_insufficent_to_checkmate}% \nof moves were checkmates", fontsize=10,bbox=dict(facecolor='red', alpha=0.5))
-plt.xlabel("amount of games")
+plt.ylabel("amount of games")
 plt.show()
 
 
@@ -45,14 +52,53 @@ plt.show()
 
 # https://www.altcademy.com/blog/how-to-change-marker-size-in-scatter-plot-matplotlib/#:~:text=In%20Matplotlib%2C%20you%20can%20change,parameter%20in%20the%20scatter%20function.&text=In%20this%20example%2C%20each%20point,1%2F72%20of%20an%20inch.
 # https://www.statology.org/line-of-best-fit-python/
+
+
 uniform_size = 10
-plt.scatter(range(len(games)), durations,s=uniform_size)
-a, b = np.polyfit(range(len(games)), durations, 1)
-plt.plot(range(len(games)), a*range(len(games))+b,linewidth=3,color='black',label="increasing time to compute the moves")  
+plt.scatter(range(len(alpha_beta_pruning)), durations2,s=uniform_size)
+a, b = np.polyfit(range(len(alpha_beta_pruning)), durations2, 1)
+
+
+plt.plot(range(len(alpha_beta_pruning)), a*range(len(alpha_beta_pruning))+b,linewidth=3,color='black',label="with alpha-beta tuning")  
+
+
 plt.xlabel("game number")
 plt.ylabel("duration (seconds)")
 plt.legend()
 plt.show()
+
+
+
+
+
+
+# CPU heating up???
+# if the amount of games increases, the time increases in aswell due to increase of heat in the CPU
+
+# https://www.altcademy.com/blog/how-to-change-marker-size-in-scatter-plot-matplotlib/#:~:text=In%20Matplotlib%2C%20you%20can%20change,parameter%20in%20the%20scatter%20function.&text=In%20this%20example%2C%20each%20point,1%2F72%20of%20an%20inch.
+# https://www.statology.org/line-of-best-fit-python/
+
+
+uniform_size = 10
+
+a, b = np.polyfit(range(len(games)), durations, 1)
+
+a2,b2 = np.polyfit(range(len(games)), durations2, 1)
+
+
+plt.plot(range(len(games)), a*range(len(games))+b,linewidth=3,color='red',label="without alpha beta pruning")  
+
+plt.plot(range(len(alpha_beta_pruning)), a2*range(len(alpha_beta_pruning))+b2,linewidth=3,color='blue',label="alpha beta pruning")  
+
+plt.xlabel("game number")
+plt.ylabel("duration (seconds)")
+plt.legend()
+plt.show()
+
+
+
+
+
 
 
 
